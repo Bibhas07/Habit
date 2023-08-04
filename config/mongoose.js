6de9e3efec9a3cd5fc37f2174c4df4139905a8f4
Bibhas <bibhasjha07@gmail.com>
2023-08-04 21:33:07 +0530

    env changed

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`${process.env.DB_MONGO}`)
+mongoose.connect(`${process.env.DB}`).then(() => {
+    console.log('Connection Successful')
+}).catch((err) => console.log(err))
 
 const db = mongoose.connection
 
